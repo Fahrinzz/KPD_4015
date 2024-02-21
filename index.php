9db089fb4a04effb5132774bcba8d1f90b2b898e
KambingFly <106731420+Fahrinzz@users.noreply.github.com>
2024-02-21 09:52:15 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <style type="text/css">
+        td{
+            padding: 5px;
+            border: 1px solid #ccc;
+        }
+    </style>
+    <body>
+        <center>
+            <h2>SENARAI NAMA</h2>
+            <?php
+            include('config.php');
+            $query1=mysqli_query($connect,"SELECT id,nama_pelajar,no_kp FROM data");
+            num_result=mysqli_num_rows($query1);
+            ?>
+        </center>
+    </body>
+</html>
+
+
